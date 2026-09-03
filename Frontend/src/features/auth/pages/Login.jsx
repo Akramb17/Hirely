1cf1603874563f300836fff261ad7cad4e8bef67
Akramb17 <akrambhura1701@gmail.com>
2026-09-04 01:03:43 +0530

    Add authentication context, hooks, and protected routes; integrate axios for API calls

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../auth.form.scss'
 import { useNavigate, Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth.js'
 
 const Login = () => {
 
-  const handleSubmit = (e) => {
+  const { loading, handleLogin } = useAuth()
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    await handleLogin({email, password})
+    navigate('/')
+  }
+
+  if (loading) {
+    return <main><h1>Loading......</h1></main>
   }
 
   return (
@@ -15,12 +28,16 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+            <input 
+            onChange={(e) => setEmail(e.target.value)}
+            type="email" id="email" name="email" placeholder="Enter your email" required />
           </div>
 
           <div className="input-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" required />
+            <input
+             onChange={(e) => setPassword(e.target.value)}
+             type="password" id="password" name="password" placeholder="Enter your password" required />
           </div>
 
           <button className="button primary-button" type="submit">Login</button>
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth.js'
 
 
 const Register = () => {
 
   const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { loading, handleRegister } = useAuth()
 
-  const handleSubmit = (e) => {
-  e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await handleRegister({ username, email, password })
+    navigate('/')
   }
+
+  if (loading) {
+    return <main><h1>Loading......</h1></main>
+  }
+
   return (
     <main>
       <div className="form-container">
@@ -16,16 +28,22 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
             <div className="input-group">
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Enter your username" required />
+            <input
+            onChange={(e) => setUsername(e.target.value)}
+            type="text" id="username" name="username" placeholder="Enter your username" required />
           </div>
           <div className="input-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+            <input
+             onChange={(e) => setEmail(e.target.value)}
+             type="email" id="email" name="email" placeholder="Enter your email" required />
           </div>
 
           <div className="input-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" required />
+            <input
+             onChange={(e) => setPassword(e.target.value)}
+             type="password" id="password" name="password" placeholder="Enter your password" required />
           </div>
 
           <button className="button primary-button" type="submit">Register</button>
