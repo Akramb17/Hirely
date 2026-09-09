bd0fffc5e806b1f3004cb4d7d4705b0cffa6e133
Akramb17 <akrambhura1701@gmail.com>
2026-09-09 18:50:56 +0530

    Enhance Puppeteer launch options for PDF generation

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -153,7 +153,10 @@ async function generateInterviewReport({
 
 async function generatePdfFromHtml(htmlContent) {
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+  headless: true,
+  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
 
   const page = await browser.newPage();
 
