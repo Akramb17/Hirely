a7189d7a5f6dfe6f2ae23bd70ffd906536f8d76b
Akramb17 <akrambhura1701@gmail.com>
2026-09-09 17:14:11 +0530

    Fix interview context import casing

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./style.scss"
-import { InterviewProvider } from './features/interview/Interview.context.jsx'
+import { InterviewProvider } from './features/interview/interview.context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
